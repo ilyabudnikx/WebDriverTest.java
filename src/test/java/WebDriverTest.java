3bc2520e69f3869e0f6270d76614102162f0f813
Ilya <ilyabudnikxs@gmail.com>
2021-11-14 22:19:34 +0300

    ver 0.0.19windows

--- src/test/java/WebDriverTest.java
+++ src/test/java/WebDriverTest.java
@@ -12,7 +12,7 @@ public class WebDriverTest {
 
                 System.setProperty("webdriver.gecko.driver","geckodriver.exe");
             WebDriver driver = new FirefoxDriver();
-            Dimension d = new Dimension(945,1020);
+            Dimension d = new Dimension(1330,1100);
             driver.get("https://bitsgap.com/sign-in/?d=app");
             driver.manage().window().setSize(d);
             System.out.print(driver.manage().window().getSize() + "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
@@ -24,26 +24,26 @@ public class WebDriverTest {
             WebElement searchAcceptButton = driver.findElement(By.cssSelector("#login-form > button"));
             searchAcceptButton.click();
             Thread.sleep(12000);
-            WebElement searchCloseButton = driver.findElement(By.cssSelector("body > div.modal > div.modal__content.modal__content_mobile > button"));
+            WebElement searchCloseButton = driver.findElement(By.cssSelector(".modal__close-button"));
             searchCloseButton.click();
 
-            WebElement searchMenuButton = driver.findElement(By.cssSelector("#root > main > div > div > div > div > div > div > div.m-bots-page__header > div.m-page-header > div:nth-child(1) > button"));
+            WebElement searchMenuButton = driver.findElement(By.cssSelector(".main-menu__button"));
             searchMenuButton.click();
             Thread.sleep(1000);
-            WebElement searchDemo = driver.findElement(By.cssSelector("body > div.MuiDrawer-root.MuiDrawer-modal > div.MuiPaper-root.MuiDrawer-paper.m-page-header__menu.MuiDrawer-paperAnchorLeft.MuiPaper-elevation16 > div > div.m-main-menu__switches > div:nth-child(1) > label"));
+            WebElement searchDemo = driver.findElement(By.cssSelector(".switch__switch"));
             searchDemo.click();
             Thread.sleep(7000);
-            WebElement searchCloseButton2 = driver.findElement(By.cssSelector("body > div.modal > div.modal__content.modal__content_mobile > button"));
+            WebElement searchCloseButton2 = driver.findElement(By.cssSelector(".modal__close-button"));
             searchCloseButton2.click();
-            WebElement searchTradingButton = driver.findElement(By.cssSelector("body > div.MuiDrawer-root.MuiDrawer-modal > div.MuiPaper-root.MuiDrawer-paper.m-page-header__menu.MuiDrawer-paperAnchorLeft.MuiPaper-elevation16 > div > div.m-main-menu__content > ul > li:nth-child(1) > div > a"));
+            WebElement searchTradingButton = driver.findElement(By.cssSelector("a.MuiButtonBase-root:nth-child(1)"));
             searchTradingButton.click();
             Thread.sleep(4000);
-            WebElement searchNicePrice = driver.findElement(By.cssSelector("#root > main > div > div > div > div > div > div > div.m-trading-page > div.m-trading-page__tab-content > div.m-trading-page__view.m-trading-page__view_trading.m-trading-page__view_visible > div.m-trading-page__place-order > div > div.place-order__content > div:nth-child(4) > div > button"));
+            WebElement searchNicePrice = driver.findElement(By.cssSelector(".best-price > button:nth-child(1)"));
             searchNicePrice.click();
-            WebElement searchValue = driver.findElement(By.cssSelector("#root > main > div > div > div > div > div > div > div.m-trading-page > div.m-trading-page__tab-content > div.m-trading-page__view.m-trading-page__view_trading.m-trading-page__view_visible > div.m-trading-page__place-order > div > div.place-order__content > div:nth-child(5) > div > div > div > div > input"));
+            WebElement searchValue = driver.findElement(By.cssSelector("div.place-order__text-input:nth-child(5) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > input:nth-child(1)"));
             searchValue.sendKeys("0.01");
-            WebElement searchAcceptButton1 = driver.findElement(By.cssSelector("#root > main > div > div > div > div > div > div > div.m-trading-page > div.m-trading-page__tab-content > div.m-trading-page__view.m-trading-page__view_trading.m-trading-page__view_visible > div.m-trading-page__place-order > div > div.place-order__content > div.place-order__button > button"));
+            WebElement searchAcceptButton1 = driver.findElement(By.cssSelector(".red-green-button_size_normal"));
             searchAcceptButton1.click();
-            driver.quit();
+             driver.quit();
     }
 }
